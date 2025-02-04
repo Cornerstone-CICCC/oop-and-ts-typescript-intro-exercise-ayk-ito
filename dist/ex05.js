7b1b106e3e6a416303c4349e95f67bf76b63a823
Ayaka Ito <a.8.32.noko.info@gmail.com>
2025-02-04 12:14:24 -0800

    done exercise

--- dist/ex05.js
+++ dist/ex05.js
@@ -1,20 +1,18 @@
+"use strict";
 // 5. Create a function named `countVowels` that takes a string as a parameter.
 //    The function should return the number of vowels (a, e, i, o, u) in the string.
 //    Use TypeScript types to ensure the parameter and return type are properly typed.
-
-function countVowels(word: string): number {
-  const vowels = ["a", "e", "i", "u", "o"];
-  const char = word.split("");
-
-  let count = 0;
-  char.forEach((e) => {
-    if (vowels.indexOf(e) !== -1) {
-      count++;
-    }
-  });
-  return count;
+function countVowels(word) {
+    const vowels = ["a", "e", "i", "u", "o"];
+    const char = word.split("");
+    let count = 0;
+    char.forEach((e) => {
+        if (vowels.indexOf(e) !== -1) {
+            count++;
+        }
+    });
+    return count;
 }
-
 // Expected output:
 console.log(countVowels("hello")); // 2
 console.log(countVowels("typescript")); // 2
